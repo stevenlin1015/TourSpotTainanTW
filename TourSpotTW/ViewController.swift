724b42eac98e6c0c07ba5e7a2c44fe19dd18c77d
林松賢 <stevenlin@linsongxiande-MacBook-Pro.local>
2018-03-13 16:02:43 +0800

    Initial Commit

---
+++ TourSpotTW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TourSpotTW
+//
+//  Created by 林松賢 on 2018/3/13.
+//  Copyright © 2018年 軟實力工作室. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
